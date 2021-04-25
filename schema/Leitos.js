624dd84c83efa2194339f20fc81ea30c565c8663
Jefferson Cardoso <jefferson.mcardoso@gmail.com>
2021-04-24 21:11:24 -0300

    Adicionando dataset de leitos hospitalares

--- schema/Leitos.js
+++ schema/Leitos.js
@@ -1,43 +1,137 @@
 cube(`Leitos`, {
-  sql: `SELECT * FROM public.leitos WHERE cep between 93300001 and 93599999`,
+  sql: `
+    SELECT 
+      codigo_ibge, 
+      nome_fantasia as nome_instituicao, 
+      tipo_unidade as tipo_instituicao,
+      tipo_gestao,
+      tipo_leito, 
+      codigo_leito,
+      quantidade, 
+      quantidade_sus, 
+      quantidade - quantidade_sus as quantidade_nao_sus,
+      mes,
+      ano
+    FROM cnes_leitos l 
+    JOIN cnes_estabelecimentos e ON(l.codigo_unidade = e.codigo_unidade)
+    JOIN ceps c on(e.cep between c.inicio_cep and c.fim_cep)
+  `,
   
   joins: {
     
   },
   
   measures: {
-    count: {
-      type: `count`
-    },
-    quantidadeLeitos: {
+    quantidade: {
       sql: `quantidade`,
       type: `sum`,
     },
     quantidadeSus: {
       sql: `quantidade_sus`,
       type: `sum`,
+    },
+    quantidadePrivado: {
+      sql: `quantidade_privado`,
+      type: `sum`,
     }
   },
   
   dimensions: {
+    codigoMunicipio: {
+      sql: `codigo_ibge`,
+      type: `number`,
+      meta: {
+        scope: {
+          type: 'city',
+          columnType: 'ibge'
+        }
+      }
+    },
+
+    ano: {
+      sql: `ano`,
+      type: `number`,
+      meta: {
+        public: true
+      }
+    },
+
+    mes: {
+      sql: `mes`,
+      type: `number`,
+      meta: {
+        public: true
+      }
+    },
+
     nomeInstituicao: {
       sql: `nome_instituicao`,
-      type: `string`
+      type: `string`,
+      meta: {
+        public: true
+      }
     },
 
-    codigoLeito: {
+    tipoInstituicao: {
+      sql: `tipo_instituicao`,
+      type: `string`,
+      meta: {
+        public: true
+      }
+    },
+
+    tipoGestao: {
+      sql: `tipo_gestao`,
+      type: `string`,
+      meta: {
+        public: true
+      }
+    },
+
+    classificacao: {
       sql: `codigo_leito`,
-      type: `string`
+      type: `string`,
+      meta: {
+        public: true
+      }
     },
     
-    tipoLeito: {
-      sql: `tipo_leito`,
-      type: `string`
+    tipo: {
+      sql: `CASE 
+        WHEN tipo_leito = 'ESPEC - CLINICO' THEN 'Clínico' 
+        WHEN tipo_leito = 'ESPEC - CIRURGICO' THEN 'Cirúrgico'
+        WHEN tipo_leito = 'OBSTETRICO' THEN 'Obstétrico'
+        WHEN tipo_leito = 'PEDIATRICO' THEN 'Pediátrico'
+        WHEN tipo_leito = 'OUTRAS ESPECIALIDADES' THEN 'Outra especialidade'
+        WHEN tipo_leito = 'HOSPITAL DIA' THEN 'Hospital-dia'
+        WHEN tipo_leito = 'COMPLEMENTAR' THEN 'Complementar'
+        ELSE 'Outro' END`,
+      type: `string`,
+      meta: {
+        public: true
+      }
     },
 
-    cep: {
-      sql: `cep`,
-      type: `number`
+    total: {
+      sql: `quantidade`,
+      type: `number`,
+      meta: {
+        public: true
+      }
+    },
+    totalSus: {
+      sql: `quantidade_sus`,
+      type: `number`,
+      meta: {
+        public: true
+      }
+    },
+    totalPrivado: {
+      sql: `quantidade_privado`,
+      type: `number`,
+      meta: {
+        public: true
+      }
     }
   },
   
